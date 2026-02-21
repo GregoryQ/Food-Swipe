c16e0ccd45b89e1710a1e0f9f87c319df350f49d
GregoryQ <gregoryq100@gmail.com>
2026-02-20 23:57:09 -0500

    Routing fix attempt one

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { IntroComponent } from './pages/intro/intro.component';
 import { RestaurantDeckComponent } from '../components/restaurant-deck/restaurant-deck.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   { path: '', component: IntroComponent },
   { path: 'restaurants', component: RestaurantDeckComponent },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
