6055aa886afcacd69cdef476dc9217b92aa7caad
GregoryQ <gregoryq100@gmail.com>
2026-02-22 21:52:13 -0500

    Revert "New build testing left swipe"
    This reverts commit ff0f1cad4efaddd0ab50efdc7646b379efe084c2.

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-M5JECXY6.mjs";import"./chunk-OG5ASPMR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-PM3CQ234.mjs";import"./chunk-OG5ASPMR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
